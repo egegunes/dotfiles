d9fee89949ca095ddbcf1db4127e48c4d8fc92e2
Ege Güneş <egegunes@gmail.com>
2019-01-13 00:04:44 +0300

    Add hostname to tmux status

--- tmux/battery.sh
+++ tmux/battery.sh
@@ -5,7 +5,7 @@ then
     now=`cat /sys/class/power_supply/BAT1/charge_now`
     full=`cat /sys/class/power_supply/BAT1/charge_full`
     out=`echo $now/$full*100 | bc -l | cut -c 1-5`
-    printf "%.f%%" $out
+    printf " %.f%%" $out
 else
     echo ""
 fi
