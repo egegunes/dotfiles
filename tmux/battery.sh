767d3a5ce7a995eef81a92e5534c5a3151911dcc
Ege Güneş <egegunes@gmail.com>
2019-01-12 23:58:48 +0300

    Fix battery script

--- tmux/battery.sh
+++ tmux/battery.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ -d /sys/class/power_supply/BAT0 ];
+if [ -d /sys/class/power_supply/BAT1 ];
 then
-    now=`cat /sys/class/power_supply/BAT0/charge_now`
-    full=`cat /sys/class/power_supply/BAT0/charge_full`
+    now=`cat /sys/class/power_supply/BAT1/charge_now`
+    full=`cat /sys/class/power_supply/BAT1/charge_full`
     out=`echo $now/$full*100 | bc -l | cut -c 1-5`
-    printf "%.f%% | " $out
+    printf "%.f%%" $out
 else
     echo ""
 fi
