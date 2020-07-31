a4f709501305c189272622590d9093f9ab2d8d41
Ege Güneş <egegunes@gmail.com>
2020-07-31 13:48:18 +0300

    scripts: inbox.sh

---
+++ scripts/inbox.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+INBOX_PATH=$HOME/INBOX
+CONFIG_PATH=$HOME/.inbox.yml
+TERMINAL=alacritty
+TERMINALOPTS="--config-file $CONFIG_PATH --command"
+
+function commit {
+    local mode=$1
+    local file=$2
+
+    cd $INBOX_PATH
+
+    git add $file
+    git commit -m "$mode $file from $HOSTNAME" &>/dev/null
+    git push
+
+    cd - &>/dev/null
+}
+
+function run_editor {
+    local tmpfile=$(mktemp --suffix=".md")
+    local timestamp=$(date +%d%m%YH%H%M%S)
+    local filename="$timestamp.md"
+
+    $EDITOR $tmpfile
+
+    if [ $? != 0 ]; then
+        echo "Discarding..."
+        exit 1
+    fi
+
+    cp $tmpfile "$INBOX_PATH/$filename"
+    commit "add" "$filename"
+}
+
+function open_terminal {
+    $TERMINAL $TERMINALOPTS bash -c '$HOME/bin/inbox.sh editor'
+}
+
+function main {
+    if [[ $1 == "terminal" ]]; then
+        open_terminal
+    elif [[ $1 == "editor" ]]; then
+        run_editor
+    else
+        echo "Unknown command: $1"
+        exit 1
+    fi
+}
+
+main $1
