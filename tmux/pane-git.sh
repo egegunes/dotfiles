a608cfcd33fc6fa66f9f80b96210ee13b92c986a
Ege Güneş <ege.gunes@kartaca.com>
2020-08-18 10:30:15 +0300

    tmux: add leading space to git status

--- tmux/pane-git.sh
+++ tmux/pane-git.sh
@@ -5,9 +5,12 @@ function branch {
 
     local branch=$(git -C $path branch 2>/dev/null | grep -E "^\*" | sed 's/* //g')
     local status=$(git -C $path status -s | awk '{print $1}' | sort | uniq -c | sed 's/^ *//g' | paste -sd '|' | sed -s 's/ //g')
+    if [[ ! -z $status ]]; then
+        status=" $status"
+    fi
 
     if [[ ! -z $branch ]]; then
-        echo "($branch $status)"
+        echo "($branch$status)"
     else
         echo ""
     fi
