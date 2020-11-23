378631ee5cf4d124e28644053b4f90bad530ff17
Ege Güneş <ege.gunes@kartaca.com>
2020-11-23 10:34:27 +0300

    tmux: allow overriding gcp project and cluster name

--- tmux/cluster.sh
+++ tmux/cluster.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-kubectl config current-context | awk -F "_" {'print $4'}
+if [ -f ~/.cluster-override.txt ]; then
+    cat ~/.cluster-override.txt
+    exit 0
+fi
+
+cluster=$(kubectl config current-context | awk -F "_" {'print $4'})
+namespace=$(kubectl config view --minify | grep -oP 'namespace: \K(.*)')
+
+echo "$cluster/$namespace"
--- tmux/gcloud.sh
+++ tmux/gcloud.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+if [ -f ~/.gcloud-override.txt ]; then
+    cat ~/.gcloud-override.txt
+    exit 0
+fi
+
 echo $(gcloud config get-value project 2>/dev/null)
